624f8c171ba7939f16f7fc32142da6512d2b968b
GudochkinK <goodok-01@hotmail.com>
2023-02-23 00:40:41 +0300

    Добавление продукта в корзину

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from users.models import User
+from products.models import Basket
 
 
 def login(request):
@@ -42,7 +42,7 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+    context = {'title': 'Store - Профиль', 'form': form, 'baskets': Basket.objects.filter(user=request.user)}
     return render(request, 'users/profile.html', context)
 
 
