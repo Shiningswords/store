d1ce17597a4c9a591c0ea82d25d3fc12330e9bf6
GudochkinK <goodok-01@hotmail.com>
2023-02-23 19:55:50 +0300

    методы корзины для отображения в профиле пользователя (сумма и количество)

--- users/views.py
+++ users/views.py
@@ -42,7 +42,8 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form, 'baskets': Basket.objects.filter(user=request.user)}
+    baskets = Basket.objects.filter(user=request.user)
+    context = {'title': 'Store - Профиль', 'form': form, 'baskets': baskets}
     return render(request, 'users/profile.html', context)
 
 
