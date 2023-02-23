1dae1b05eee7ba37600870d6abc065dbe5fc301c
GudochkinK <goodok-01@hotmail.com>
2023-02-23 20:28:11 +0300

    добавление декоратора login_required (необходимость в регистрации)

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from products.models import Product, ProductCategory, Basket
+from django.contrib.auth.decorators import login_required
 from users.models import User
 
 
@@ -20,6 +21,7 @@ def products(request):
     return render(request, 'products/products.html', context)
 
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -33,6 +35,7 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
